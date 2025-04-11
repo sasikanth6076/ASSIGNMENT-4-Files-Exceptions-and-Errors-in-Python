3906263a35562291de621c0aacd4306056e0dc13
sasikanth6076 <chowdhary6076@gmail.com>
2025-04-11 16:24:51 +0530

    deleted task 1

--- Task 1: Read a File and Handle Errors.py
+++
@@ -1,7 +0,0 @@
-try:
-    with open("sample.txt","r") as file:
-        for idx, line in enumerate(file, start = 1):
-            print(f"Line {idx} : {line.strip()}")
-
-except FileNotFoundError:
-    print(f'the file sample.txt was not found')
